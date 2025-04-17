c9a889f2d971f62c00fa9ec9adb52c99444d8f2d
Aeklr <vocacze@gmail.com>
2025-04-17 15:29:37 +0200

    minor changes

--- to-do.py
+++ to-do.py
@@ -6,7 +6,7 @@ def main():
     parser = argparse.ArgumentParser(description="Simple To-Do List")
     parser.add_argument('-v', '--view', help='View the active Tasks of the To-Do List', action='store_true')
     parser.add_argument('-a', '--add', help='Add a task into the To-Do List')
-    parser.add_argument('-r', '--remove', help='Remove task from the To-Do List')
+    parser.add_argument('-r', '--remove', help='Remove task from the To-Do List, Format: -r <Task NUMBER>')
     parser.add_argument('-rA', '--remove-all', help='Remove all tasks from the To-Do List', action='store_true')
 
     # Check if no arguments are provided
